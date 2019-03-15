3237d5c91e2723152d151f209985cdde00e01606
olekdev3 <heoleksandr@gmail.com>
2019-03-15 16:44:18 +0530

    third commit

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'simple_history',
     'crispy_forms',
     'user.apps.UserConfig',
     'django.contrib.admin',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'simple_history.middleware.HistoryRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- user/models.py
+++ user/models.py
@@ -2,7 +2,38 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from simple_history.models import HistoricalRecords
 
 # Create your models here.
+
+
 class Patient(models.Model):
-    uid=models.IntegerField()
+    uid=models.IntegerField(primary_key=True)
+    name=models.TextField()
+    gender=models.TextField()
+    dob=models.TextField()
+    house=models.TextField()
+    street=models.TextField()
+    landmark=models.TextField()
+    postoffice=models.TextField()
+    district=models.TextField()
+    state=models.TextField()
+    pin_code=models.IntegerField()
+    #details=models.ForeignKey(Details,on_delete=models.CASCADE)
+
+class Details(models.Model):
+    aadhar=models.IntegerField(primary_key=True)
+    doctor=models.TextField()
+    diagnosis=models.TextField()
+    recommendation=models.TextField()
+    medicine=models.TextField()
+    test=models.TextField()
+    history=HistoricalRecords()
+
+
+
+
+
+
+
+    
